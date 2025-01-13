95f8fcb637133aad928964cfcd37f4cb31d254d3
bluke127 <dnehdrkd123@naver.com>
2025-01-14 00:29:53 +0900

    [feat]평균점수 연습중

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-###########################
-# 라이브러리 사용
-import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
 
-###########################
-# 파일로부터 데이터 읽어오기
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/lemonade.csv'
-레모네이드 = pd.read_csv(파일경로)
+# 공부 시간 (랜덤 데이터 생성)
+# np.random.seed()를 설정하는 이유는 **난수 생성의 재현성(reproducibility)**을 보장하기 위해서입니다. 재현성이란, 동일한 코드와 데이터를 사용할 때 항상 같은 결과를 얻을 수 있도록 하는 것을 의미합니다.
+np.random.seed(42)
+# 첫번째 파라미터는 난수의 최소값
+# 두번짼느 최댓값
+# 세번째는 몇개생성할지
+study_hours = np.random.uniform(1, 10, 100)  # 공부 시간: 1~10시간
+print(study_hours)
+print(np)
+# 중간고사 성적 (노이즈 추가)
+scores = 5 * study_hours + np.random.normal(0, 5, size=100)  # y = 5x + noise
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
-보스턴 = pd.read_csv(파일경로)
+# 데이터 시각화
+plt.scatter(study_hours, scores)
+plt.xlabel("Study Hours")
+plt.ylabel("Scores")
+plt.title("Study Hours vs Scores")
+plt.show()
+# Sequential 모델 생성
+model = Sequential([
+    Dense(1, input_shape=(1,), activation='linear')  # 입력 1개, 출력 1개
+])
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv'
-아이리스 = pd.read_csv(파일경로)
+# 모델 컴파일
+model.compile(optimizer='sgd', loss='mse')  # 옵티마이저: SGD, 손실 함수: MSE
 
-###########################
-# 데이터의 모양확인
-print(레모네이드.shape)
-print(보스턴.shape)
-print(아이리스.shape)
-
-###########################
-# 데이터 칼럼이름 확인
-print(레모네이드.columns)
-print(보스턴.columns)
-print(아이리스.columns)
-
-###########################
-# 독립변수와 종속변수 분리
-독립 = 레모네이드[['온도']]
-종속 = 레모네이드[['판매량']]
-print(독립.shape, 종속.shape)
+history = model.fit(study_hours, scores, epochs=50, verbose=1)
