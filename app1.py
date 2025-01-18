284b0b23f4ae9d915b15df69f304d860b2b9ed73
bluke127 <dnehdrkd123@naver.com>
2025-01-18 15:19:21 +0900

    [feat]보스턴 집값예측

--- app1.py
+++ app1.py
@@ -1,31 +1,39 @@
-# 판다스
-# 데이터를 표로 만드는 라이브러리
-# - 파일 읽어오기 : pd.read_csv(경로)
-# - 모양 확인하기 : print(데이터.shape)
-# - 칼럼 선택하기 : 데이터[['칼럼명1', '칼럼명2', '칼럼명3']]
-# - 칼럼 이름 출력하기 : print(데이터.columns)
+###########################
+# 라이브러리 사용
 import tensorflow as tf
 import pandas as pd
-파일경로 = "https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/lemonade.csv"
-데이터 = pd.read_csv(파일경로)
-# //기본적으로 5개 출력
-데이터.head()
-독립 = 데이터[['온도']]
-종속 = 데이터[['판매량']]
-# .shape는 Dataframe(행과열로 구성)의 행과열을 반환
-print(데이터.columns)
+
+###########################
+# 1.과거의 데이터를 준비합니다.
+파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
+보스턴 = pd.read_csv(파일경로)
+print(보스턴.columns)
+보스턴.head()
+
+# 독립변수, 종속변수 분리
+독립 = 보스턴[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax',
+          'ptratio', 'b', 'lstat']]
+종속 = 보스턴[['medv']]
 print(독립.shape, 종속.shape)
-#독립변수의 개수
-x = tf.keras.layers.Input(shape=[1])
-#종속변수의 개수
-y = tf.keras.layers.Dense(1)(x)
-model = tf.keras.models.Model(x, y)
-#lose값은 모델이 얼마나 좋냐 안좋냐로 보면됨
-#0에 가까워야 정답을 맞힘
+
+###########################
+# 2. 모델의 구조를 만듭니다
+X = tf.keras.layers.Input(shape=[13])
+Y = tf.keras.layers.Dense(1)(X)
+model = tf.keras.models.Model(X, Y)
 model.compile(loss='mse')
-#epochs 시도횟수
-#verbose 출력횟수
-# 처음에 10만 햇을때는 loss가 1000이었지만 10000 돌린후 다시 확인해며면 2.23로 상당히 낮아진걸 볼 수 있다
-model.fit(독립,종속,epochs=10000,verbose=0)
-model.predict(독립)
-model.predict([15])
+
+###########################
+# 3.데이터로 모델을 학습(FIT)합니다.
+model.fit(독립, 종속, epochs=1000, verbose=0)
+model.fit(독립, 종속, epochs=10)
+
+###########################
+# 4. 모델을 이용합니다
+print(model.predict(독립[5:10]))
+# 종속변수 확인
+print(종속[5:10])
+
+###########################
+# 모델의 수식 확인
+print(model.get_weights())
