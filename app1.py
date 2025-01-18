15ce6f13ab24e9c23f3a3b0a8fe161fe95cba51b
bluke127 <dnehdrkd123@naver.com>
2025-01-19 02:05:01 +0900

    [feat]아이리스
    시그모이드

--- app1.py
+++ app1.py
@@ -1,29 +1,34 @@
-###########################
+##########################
 # 라이브러리 사용
 import tensorflow as tf
 import pandas as pd
 
 ###########################
 # 1.과거의 데이터를 준비합니다.
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
-보스턴 = pd.read_csv(파일경로)
-print(보스턴.columns)
-보스턴.head()
+파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv'
+아이리스 = pd.read_csv(파일경로)
+아이리스.head()
+print(아이리스, )
 
-# 독립변수, 종속변수 분리
-독립 = 보스턴[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax',
-          'ptratio', 'b', 'lstat']]
-#medv는 집값 중앙값
-종속 = 보스턴[['medv']]
-print(독립.shape, 종속.shape)
+# 원핫인코딩
+# 문자만 원핫인 코딩에 적용
+아이리스 = pd.get_dummies(아이리스)
+
+print(아이리스, '원핫인코딩')
+# 종속변수, 독립변수
+독립 = 아이리스[['꽃잎길이', '꽃잎폭', '꽃받침길이', '꽃받침폭']]
+종속 = 아이리스[['품종_setosa', '품종_versicolor', '품종_virginica']]
+print(아이리스, 독립.shape, 종속.shape)
 
 ###########################
 # 2. 모델의 구조를 만듭니다
-X = tf.keras.layers.Input(shape=[13])
-Y = tf.keras.layers.Dense(1)(X)
+X = tf.keras.layers.Input(shape=[4])
+# 범주문제기 때문에 softmax
+Y = tf.keras.layers.Dense(3, activation='softmax')(X)
 model = tf.keras.models.Model(X, Y)
-model.compile(loss='mse')
 
+model.compile(loss='categorical_crossentropy',
+              metrics=['accuracy'])
 ###########################
 # 3.데이터로 모델을 학습(FIT)합니다.
 model.fit(독립, 종속, epochs=1000, verbose=0)
@@ -31,10 +36,14 @@ model.fit(독립, 종속, epochs=10)
 
 ###########################
 # 4. 모델을 이용합니다
-print(model.predict(독립[5:10]))
-# 종속변수 확인
-print(종속[5:10])
+# 맨 처음 데이터 5개
+print(model.predict(독립[:5]))
+print(종속[:5])
+
+# 맨 마지막 데이터 5개
+print(model.predict(독립[-5:]))
+print(종속[-5:])
 
 ###########################
-# 모델의 수식 확인
+# weights & bias 출력
 print(model.get_weights())
